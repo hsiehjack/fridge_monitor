fbe7d3b74b69c5f00f15a4e6b0d45d1a95ce3969
jakeowns <beanshield@gmail.com>
2013-11-19 00:05:51 -0800

    Update getJSON.php

--- www/getJSON.php
+++ www/getJSON.php
@@ -6,20 +6,10 @@ mysql_select_db('pi') or die('select db error');
 $query = 'SELECT DATE, TEMP FROM DATETEMP';
 $result = mysql_query($query) or die(mysql_error());
 
-$table = array(
-    'cols' => array(
-        array('label' => 'date', 'type' => 'timestamp'),
-        array('label' => 'temp', 'type' => 'number')
-    ),
-    'rows' => array()
-);
+$table = array();
+
 while ($row = mysql_fetch_assoc($result)) {
-    $table['rows'][] = array(
-        'c' => array(
-            array('v' => $row['date']),
-            array('v' => $row['temp'])
-        )
-    );
+    push_array($table, $row);
 }
 mysql_close($link);
 echo json_encode($table); 
