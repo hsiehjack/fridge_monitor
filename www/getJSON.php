c130d288b18239004d53c41b274b3bb0da5b4dac
Jack Hsieh <jack@egenware.com>
2013-11-20 14:05:25 -0800

    getJSON.php: wrong json format

--- www/getJSON.php
+++ www/getJSON.php
@@ -3,51 +3,49 @@ $style = $_GET['style'];
 
 $con = mysqli_connect('localhost', 'pi', 'pi', 'pi');
 if (!$con) {
-	die('Cloud not connect: ' . mysqli_error($con));
+	die('Could not connect: ' . mysqli_error($con));
 }
-
 mysqli_select_db($con, 'pi');
-
 abstract class chart_api {
 	abstract public function set_cols();
 	abstract public function set_rows($result);
 	protected $json;
 	public function get_json() {
-		return json_encode($this->$json);
+		return json_encode($this->json);
 	}
 }
 class gauge extends chart_api {
 	public function set_cols() {
-		$this->$json['cols'] = array(
+		$this->json['cols'] = array(
 		    array('id' => 'Label', 'label' => 'Label', 'type' => 'string'),
 		    array('id' => 'Value', 'label' => 'Value', 'type' => 'number')
 		);
 	}
 	public function set_rows($result) {
 		while($row = mysqli_fetch_array($result)){ 
-			$rows[] = array('c' => 
+			$rows[] = array('c' => array(
 			array('v' => "Temperature"),
 			array('v' => $row['temp'])
-			);
+			));
 		}
-		$this->$json['rows'] = $rows;
+		$this->json['rows'] = $rows;
 	}
 }
 class graph extends chart_api {
 	public function set_cols() {
-		$this->$json['cols'] = array(
+		$this->json['cols'] = array(
 		array('id' => 'DateTime', 'label' => 'DateTime', 'type' => 'string'),
 		array('id' => 'Temp', 'label' => 'Temp', 'type' => 'number')
 		);
 	}
 	public function set_rows($result) {
 		while($row = mysqli_fetch_array($result)){ 
-			$rows[] = array('c' => 
+			$rows[] = array('c' => array(
 			array('v' => $row['date']),
 			array('v' => $row['temp']),
-			);
+			));
 		}
-		$this->$json['rows'] = $rows;
+		$this->json['rows'] = $rows;
 	}
 }
 switch ($style) {
@@ -60,7 +58,7 @@ switch ($style) {
 		break;
 
 	case "graph":
-		$sql="SELECT date, temp FROM datetemp ORDER BY id DESC LIMIT 100";
+		$sql="SELECT date, temp FROM datetemp ORDER BY id LIMIT 100";
 		$result = mysqli_query($con, $sql);
 		$obj = new graph;
 		$obj->set_cols();
