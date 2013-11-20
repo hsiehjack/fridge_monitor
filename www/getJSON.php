93bd2b542985d4b2d1b5cdcfb5d8b9338ff8d0d3
jakeowns <beanshield@gmail.com>
2013-11-19 17:57:50 -0800

    New json format.

--- www/getJSON.php
+++ www/getJSON.php
@@ -12,20 +12,18 @@ switch ($style) {
 	case "gauge":
 		$sql="SELECT temp FROM datetemp ORDER BY id DESC LIMIT 1";
 		$result = mysqli_query($con, $sql);
-		$table = array(
-    		'cols' => array(
-        		'id' => 'Label', 'label' => 'Label', 'type' => 'string',
-        		'id' => 'Value', 'label' => 'Value', 'type' => 'number'
-    		),
-    		'rows' => array()
+
+		$table['cols'] = array(
+		    array('id' => 'Label', 'label' => 'Label', 'type' => 'string'),
+		    array('id' => 'Value', 'label' => 'Value', 'type' => 'number')
 		);
-		while ($row = mysqli_fetch_assoc($result)) {
-    		$table['rows'][] = 
-        		'c' => array(
-            		'v' => $row['date'],
-            		'v' => $row['temp']
-  	 		);
+		while($row = mysqli_fetch_array($result)){ 
+			$data = array();
+			$data[] = array('v' => $row['date']);
+			$data[] = array('v' => $row['temp'];
+			$rows[] = array('c' => $data);
 		}
+		$table['rows'] = $rows;
 		break;
 
 	case "graph":
